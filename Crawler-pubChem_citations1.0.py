8fb256ec9e4e6272183b6f68410824a98fbfc0ec
wuyou0923 <68952444+wuyou0923@users.noreply.github.com>
2021-06-22 08:47:18 +0800

    Update Crawler-pubChem_citations1.0.py

--- Crawler-pubChem_citations1.0.py
+++ Crawler-pubChem_citations1.0.py
@@ -1,13 +1,13 @@
-from selenium import webdriver  # 用来驱动浏览器的
-from selenium.webdriver.common.by import By  # 按照什么方式查找，By.ID,By.CSS_SELECTOR
-from selenium.webdriver.support import expected_conditions as EC  # 和下面WebDriverWait一起用的
-from selenium.webdriver.support.wait import WebDriverWait  # 等待页面加载某些元素
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 import xlrd
 # import xlwt
 import requests
 driver=webdriver.Chrome()
-data = xlrd.open_workbook('sourceFile/待补充的5个化合物.xlsx')
+data = xlrd.open_workbook('sourceFile/Compound list.xlsx')
 table = data.sheet_by_index(0)
 pert_id = table.col_values(0)
 pert_id_name = table.col_values(1)
@@ -46,7 +46,7 @@ while True:
         #currrntUrl=driver.current_url
         #time.sleep(1)
         #driver.get(currrntUrl+"#section=CAS")
-        current_url=driver.current_url#获取当前页面的网址
+        current_url=driver.current_url
         time.sleep(2)
         #WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CLASS_NAME, "p-sm-top")))
         #name_div = driver.find_element_by_class_name("p-sm-top").get_attribute("textContent")
